5cb4349dc726f4c1a50fbcee18f3f25778256640
csb6 <cblakley15@gmail.com>
2018-03-15 18:45:51 -0700

    Got ui working, loading starting game field from file
    added new Constants file
    added new style configurations for difference tiles

---
+++ lib/Constants.rb
@@ -0,0 +1,7 @@
+module Constants
+	TITLE = "Project 1"
+	BACKGROUND = "light blue"
+	WIDTH = "1920"
+	HEIGHT = "1000"
+	LEVEL_PATH = "/users/kevinblakley/eclipse-workspace/project-1/lib/ui/levels/island0.csv"
+end
--- lib/ui/GameboardManager.rb
+++ lib/ui/GameboardManager.rb
@@ -1,18 +1,22 @@
 require 'tk'
+require 'csv'
 require_relative '../engine/classes/GameObject'
 require_relative '../engine/classes/InteractiveObject'
+require_relative '../Constants'
 
-TITLE = "Project 1"
-BACKGROUND = "blue"
-WIDTH = "900"
-HEIGHT = "900"
+#Main UI file, adds/removes buttons, monitors for input
 
-jim = Soldier.new("Jim",0,50,40)
+Tk::Tile::Style.configure('Ocean.TLabel', {"font" => "helvetica 17", "background" => "blue", "foreground" => "blue", "width" => 4, "justify" => "right"} )
+Tk::Tile::Style.configure('Land.TButton', {"font" => "helvetica 12", "background" => "tan", "foreground" => "tan", "width" => 1} )
+Tk::Tile::Style.configure('Soldier.TButton', {"font" => "helvetica 12", "background" => "light tan", "width" => 1} )
+
+#jim = Soldier.new("Jim",0,50,40)
 
 root = TkRoot.new() do
-  title TITLE
-  background BACKGROUND
-  minsize(WIDTH, HEIGHT)
+  title Constants::TITLE
+  background Constants::BACKGROUND
+  minsize(Constants::WIDTH, Constants::HEIGHT)
+  resizable true, true
 end
 
       menu = TkFrame.new(root) { grid('row' => 0, 'column' => 0) }
@@ -23,5 +27,73 @@ end
               grid('row' => 0, 'column' => 0)
             end
             
+            TkButton.new(menu) do
+            		text "Load Game"
+            		
+            		command do
+            			loadFile = Tk::getOpenFile
+            		end
+            		grid('row' => 0, 'column' => 2)
+            end
+            
+            TkButton.new(menu) do
+            		text "Save Game"
+            		
+							command do
+								saveFile = Tk::getSaveFile
+							end
+							grid('row' => 0, 'column' => 1)
+					 end
 
+			gameField = TkFrame.new(root){ grid('row' => 1, 'column' => 0) }
+
+def initField(gameField) #When game starts, lays out all tiles as stored in the save
+	
+	rowArray = CSV.read(Constants::LEVEL_PATH, :col_sep => "	" )
+
+	r = 0
+	rowArray.each do |row|
+		c = 0
+		row.each do |letter|
+			
+			case letter
+				when'o' #Ocean tile
+					Tk::Tile::Label.new(gameField) do
+						style "Ocean.TLabel"
+						text "~~"
+						grid('row' => r, 'column' => c )
+					end
+					
+				when 'l' #Land tile
+					Tk::Tile::Button.new(gameField) do
+						style "Land.TButton"
+						text "**"
+						grid('row' => r, 'column' => c)
+					end
+					
+				when 's' #Soldier tile
+					Tk::Tile::Button.new(gameField) do
+						style "Soldier.TButton"
+						text "S"
+						grid('row' => r, 'column' => c )
+					end
+			end
+			
+			c += 1
+		end
+		r += 1
+	end
+	
+	
+end
+
+def updateField
+	
+end
+
+def saveField
+	
+end
+
+initField(gameField)
 Tk.mainloop
