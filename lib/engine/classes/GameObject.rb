14325768f58acc0e3b0844783c618bf30522d254
csb6 <cblakley15@gmail.com>
2018-04-25 16:04:43 -0700

    Made conditional code on button press more modular, added comments

--- lib/engine/classes/GameObject.rb
+++ lib/engine/classes/GameObject.rb
@@ -30,44 +30,64 @@ class GameObject
 		@isBlueTeam = true
 		
 		@image.bind("1", proc {
-			@selectManager.nameLabel.value = @objectName #Sets name of nameLabel on sidebar
-			
-			if @canShoot
-				@selectManager.healthLabel.value = "Health: #{@health}"
-				@selectManager.ammoLabel.value = "Ammo: #{@ammo}"
-				@selectManager.coverLabel.value = "Cover: #{@coverMod}"
-			else
-				@selectManager.healthLabel.value = "Health: "
-				@selectManager.ammoLabel.value = "Ammo: "
-				@selectManager.coverLabel.value = "Cover: "
-			end
-			
-			
-			if !@selectManager.isCurrentSet && @isMovable #If no current yet and this tile = movable
-				if @isBlueTeam === @selectManager.isBlueTurn #make it the current if it is one of current player's men
-					@selectManager.currentTraits = self
-					@selectManager.isCurrentSet = true
-				end
-				
-			elsif !@selectManager.isTargetSet && @selectManager.isCurrentSet #If no tile target yet, but current is
-				if @selectManager.inShootingMode #If looking for a shooting target and this tile can shoot, make it a target
-					if @canShoot
-						@selectManager.targetTraits = self
-						@selectManager.isTargetSet = true
-					end
-				else #If not looking for shooting target, see if its within 5 tiles, set it as target if it is
-					distanceToCurrent = GraphMath.distanceFormula(@selectManager.currentTraits.xPos, @selectManager.currentTraits.yPos, @xPos, @yPos)
-					if @isOccupiable &&  distanceToCurrent <= 5
-						@selectManager.targetTraits = self
-						@selectManager.isTargetSet = true
-						@selectManager.inMovingMode = true
-					end
-				end
-				#@selectManager.targetTraits = @traits
-				#@selectManager.isTargetSet = true
-			end
+			updateLabels
+			performAction
 		})
 	end
+	
+	
+	def updateLabels #on the side menu, for selected item
+		@selectManager.nameLabel.value = @objectName #Sets name of nameLabel on sidebar
+							
+		if @canShoot
+			@selectManager.healthLabel.value = "Health: #{@health}"
+			@selectManager.ammoLabel.value = "Ammo: #{@ammo}"
+			@selectManager.coverLabel.value = "Cover: #{@coverMod}"
+		else
+			@selectManager.healthLabel.value = "Health: "
+			@selectManager.ammoLabel.value = "Ammo: "
+			@selectManager.coverLabel.value = "Cover: "
+		end
+	end
+		
+		
+	def performAction #Makes the soldier move or shoot something, checking what is selected, if target/current positions are set
+
+		if !@selectManager.isCurrentSet && @isMovable #If no current yet and this tile = movable
+			setCurrent
+			
+		elsif !@selectManager.isTargetSet && @selectManager.isCurrentSet #If no tile target yet, but current is
+			setTarget
+		end
+	end
+	
+	
+	def setCurrent #tile to be this tile if it's the turn of the tile's team
+		if @isBlueTeam === @selectManager.isBlueTurn
+			@selectManager.currentTraits = self
+			@selectManager.isCurrentSet = true
+		end
+	end
+	
+	
+	def setTarget #tile to be this tile if it's the turn of the tile's team
+		if @selectManager.inShootingMode #If looking for a shooting target and this tile can shoot, make it a target
+			if @canShoot
+				@selectManager.targetTraits = self
+				@selectManager.isTargetSet = true
+			end
+		else #If not looking for shooting target, see if this tile is within 5 tiles, set it as target if it is
+			distanceToCurrent = GraphMath.distanceFormula(@selectManager.currentTraits.xPos, @selectManager.currentTraits.yPos, @xPos, @yPos)
+			if @isOccupiable &&  distanceToCurrent <= 5
+				@selectManager.targetTraits = self
+				@selectManager.isTargetSet = true
+				@selectManager.inMovingMode = true
+			end
+		end
+		#@selectManager.targetTraits = @traits
+		#@selectManager.isTargetSet = true
+	end
+	
 	
 	def setPosition(x, y, a, b)
 		@xPos = x
@@ -76,6 +96,7 @@ class GameObject
 		@y1 = b
 		@image.coords(@x1, @y1)
 	end
+	
 	
 	def flashImage(pic)
 		origImage = @image[:image]
