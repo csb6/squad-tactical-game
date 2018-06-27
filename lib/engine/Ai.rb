3f79994883a99e0d3e561bb79bf99ec2e71529e5
csb6 <cblakley15@gmail.com>
2018-06-26 22:26:45 -0700

    Added basic methods for finding nearest enemy, best attack location

--- lib/engine/Ai.rb
+++ lib/engine/Ai.rb
@@ -3,17 +3,57 @@ class Ai
         print "Prepare to lose, human. I am initializing..."
         @field = field
         @selectManager = selectionManager
+
         field.findAllSoldiers
+        @walls = PathFind.getWalls
         @blueSoldiers = field.findSoldiers(true)
         @redSoldiers = field.findSoldiers(false)
+        @mainSoldier = @redSoldiers[0] #Sgt Kolochev
+
         print "Done initializing.\n"
     end
 
     def takeTurn
         @owSoldiers = @field.findAllInOW(true)
-        target = @owSoldiers[0]
-        targetPos = [ target.xPos+1, target.yPos-1 ]
-        FieldUtils.autoMove([10,2], targetPos, @field, @selectManager)
+
+        startPos = @mainSoldier.getCoords
+        target = findNearestEnemy(@mainSoldier)
+        targetPos = target.getCoords
+
+        attackPos = findAttackPos(targetPos)
+        puts "Target: #{target.objectName}. Planned attack Pos: #{attackPos}"
+        FieldUtils.autoMove(startPos, attackPos, @field, @selectManager)
+    end
+
+    def findNearestEnemy(tile)
+        tilePos = tile.getCoords
+        nearest = nil
+        @blueSoldiers.each do |soldier|
+            soldierPos = soldier.getCoords
+            distance = GraphMath.distanceFormula( soldierPos[0], soldierPos[1], tilePos[0], tilePos[1] )
+            if nearest === nil 
+                nearest = [ soldier, distance ]
+            elsif distance < nearest[1]
+                nearest = [ soldier, distance ]
+            end
+        end
+        return nearest[0]
+    end
+
+    def findAttackPos(targetPos)
+        foundPos = false
+        x = 5
+        y = -5
+        while !foundPos
+            candidatePos = [ targetPos[0]+x, targetPos[1]+y ]
+            if @field.checkIfOccup(candidatePos)
+                foundPos = true
+            else
+                x += 1
+                y -= 1
+            end
+        end
+        return candidatePos
     end
 
     def test #Tests that arrays of useful tiles are updated
